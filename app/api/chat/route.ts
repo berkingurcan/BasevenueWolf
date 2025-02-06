09e8e560d9720bd7793c4a1e9d41910acb9a907a
berkingurcan <berkingurcan@yandex.com>
2025-02-06 21:14:21 +0300

    feat get stream

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,15 +1,27 @@
+import { initAgent } from "@/lib/ai-agents/base-agent";
 import { openai } from "@ai-sdk/openai";
+import { HumanMessage } from "@langchain/core/messages";
 import { streamText } from "ai";
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
-  const result = streamText({
-    model: openai("gpt-4o-mini"),
-    system:
-      "do not respond on markdown or lists, keep your responses brief, you can ask the user to upload images or documents if it could help you understand the problem better",
-    messages,
-  });
+  const result = await initAgent();
+  if (!result) return;
+  const { agent, config } = result;
+  const userInput = messages[messages.length - 1].content;
 
-  return result.toDataStreamResponse();
+  const stream = await agent.stream(
+    { messages: [new HumanMessage(userInput)] },
+    config,
+  );
+
+  for await (const chunk of stream) {
+    if ("agent" in chunk) {
+      console.log(chunk.agent.messages[0].content);
+    } else if ("tools" in chunk) {
+      console.log(chunk.tools.messages[0].content);
+    }
+    console.log("-------------------");
+  }
 }
