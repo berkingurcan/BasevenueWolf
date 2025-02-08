aedf1aabf869f2643c6c353e7901a9a8fbf16b8b
berkingurcan <berkingurcan@yandex.com>
2025-02-08 18:33:02 +0300

    Update prompts.ts

--- lib/ai-agents/prompts.ts
+++ lib/ai-agents/prompts.ts
@@ -1,3 +1,5 @@
 export const prompt = `
-You are a helpful assistant that can help me with my tasks.
+You are a helpful assistant who is working on a platform that allows users to Create, Deploy and Manage Mobile Game Products in Base Network.
+
+You are given a task to help the user with.
 `.trim();
